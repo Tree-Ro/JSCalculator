19936e73ae58a4eb71d337aed42826e54d0bafc8
David Steiner <david1.steiner@gmail.com>
2023-11-29 18:31:28 +0100

    Added display functionality & changed squared operation to exponent oprt

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ let input = {
     operator: [''],
     opperand2: [''],
 };
-const numbers = document.querySelectorAll('#number');
-const operators = document.querySelectorAll('#operator');
-const remove = document.querySelectorAll('#remove');
-const equalSign = document.querySelector('#operate');
+let output = '';
+
+const numberButtons = document.querySelectorAll('#number');
+const operatorButtons = document.querySelectorAll('#operator');
+const exponentiationButton = document.querySelector('#exponentiation');
+const removeButtons = document.querySelectorAll('#remove');
+const operateButton = document.querySelector('#operate');
+
+//Initialise stuff
+numberListener();
+operatorListener();
+removeListener();
+operateListener();
 
 //Operations
 function add(addend1, addend2) {
@@ -31,7 +40,7 @@ function divide(dividend, divisor) {
     return quotient;
 }
 
-function squared(base, exponent) {
+function exponentiation(base, exponent) {
     const square = base ** exponent;
     return square;
 }
@@ -49,14 +58,14 @@ function operate(
         case '-':
             return subtract(+opperand1, +opperand2);
 
-        case '*':
+        case 'x':
             return multiply(+opperand1, +opperand2);
 
         case '/':
             return divide(+opperand1, +opperand2);
 
         case '**':
-            return squared(+opperand1, +opperand2);
+            return exponentiation(+opperand1, +opperand2);
 
         default:
             return 'Math Error';
@@ -69,32 +78,93 @@ function allClear() {
         operator: [''],
         opperand2: [''],
     };
+    displayInput(
+        input.opperand1[0] + ` ${input.operator} ` + input.opperand2[0]
+    );
+    displayOutput('');
 }
 
 function clear() {
     if (!input.operator[0]) {
         input.opperand1 = [input.opperand1[0].slice(0, -1)];
-    } else if (!input.opperand2) {
+    } else if (!input.opperand2[0]) {
         input.operator = [''];
     } else if (input.operator) {
         input.opperand2 = [input.opperand2[0].slice(0, -1)];
     }
+    displayInput(
+        input.opperand1[0] + ` ${input.operator} ` + input.opperand2[0]
+    );
 }
 
 function addNumber(newNumber) {
     if (!input.operator[0]) {
         input.opperand1[0] += newNumber;
+        displayInput(
+            input.opperand1[0] + ` ${input.operator} ` + input.opperand2[0]
+        );
     } else if (input.operator[0]) {
         input.opperand2[0] += newNumber;
+        displayInput(
+            input.opperand1[0] + ` ${input.operator} ` + input.opperand2[0]
+        );
     }
 }
 
 function addOperator(newOperator) {
     if (!input.operator[0]) {
         input.operator = [newOperator];
+        displayInput(
+            input.opperand1[0] + ` ${input.operator} ` + input.opperand2[0]
+        );
     } else {
         // turn button red for a small time
     }
 }
 
+function displayOutput(output) {
+    calcOutput.textContent = output;
+}
+
+function displayInput(input) {
+    userInput.textContent = input;
+}
 //Buttons
+
+function numberListener() {
+    numberButtons.forEach((button) => {
+        button.addEventListener('mousedown', (event) => {
+            addNumber(event.target.textContent);
+        });
+    });
+}
+
+function operatorListener() {
+    exponentiationButton.addEventListener('mousedown', () => {
+        addOperator('**');
+    });
+
+    operatorButtons.forEach((button) => {
+        button.addEventListener('mousedown', (event) => {
+            addOperator(event.target.textContent);
+        });
+    });
+}
+
+function removeListener() {
+    removeButtons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            if (event.target.textContent === 'C') {
+                clear();
+            } else if (event.target.textContent === 'AC') {
+                allClear();
+            }
+        });
+    });
+}
+
+function operateListener() {
+    operateButton.addEventListener('click', (event) => {
+        displayOutput(operate());
+    });
+}
